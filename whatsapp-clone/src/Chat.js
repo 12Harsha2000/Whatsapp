309d540b10a53bde078dc9f66f0df33aa76446f3
12Harsha2000 <harshamariajose.20@gmail.com>
2023-10-08 21:33:07 +0530

    Initiated

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Chat.css";
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 
 function Chat() {
- return(
-  <div className='chat'>
-
+  const [seed, setSeed] = useState(""); 
+  return(
+   <div className='chat'>
     <div className="chat_header">
-<Avatar />
+     <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+     <div className="chat_headerInfo">
+      <h3>Room Name</h3>
+      <p>Last seen at...</p>
+     </div>
+
+     <div className="chat_headerRight">
+      <IconButton>
+        <SearchOutlined />
+      </IconButton>
+      <IconButton>
+        <AttachFile />
+      </IconButton>
+      <IconButton>    
+        <MoreVert />
+      </IconButton>
+      </div>
     </div>
 
-    <div className="chat_body">
+    <div className="chat_body"></div>
 
-    </div>
-
-    <div className="chat_footer">
-
-    </div>
-
-  </div>
+    <div className="chat_footer"></div>
+   </div>
  );
 }
 
