b0d3e6478e80e65d5442876bd9ca720bf7d2b979
12Harsha2000 <harshamariajose.20@gmail.com>
2023-10-08 08:50:42 +0530

    Initiated

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDXVlRIhHEEaBhLJVeLhApJgxPIv00EWX0",
+    authDomain: "whatsapp-clone-2ac4e.firebaseapp.com",
+    projectId: "whatsapp-clone-2ac4e",
+    storageBucket: "whatsapp-clone-2ac4e.appspot.com",
+    messagingSenderId: "816726071264",
+    appId: "1:816726071264:web:efc8b1d190ff1ecd7b3d85",
+    measurementId: "G-PM6ZS9BYEX"
+  };
