24a0aaee7d130fc3a9919d16465cfc179f5eb50c
12Harsha2000 <harshamariajose.20@gmail.com>
2023-10-08 12:30:02 +0530

    Initiated

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 import './Sidebar.css';
 
 function Sidebar() {
@@ -11,13 +12,22 @@ function Sidebar() {
      <div className="sidebar_header">
       <Avatar />
       <div className="sidebar_headerRight">
-        <DonutLargeIcon />
-        <ChatIcon />
-        <MoreVertIcon />
-
+        <IconButton>
+           <DonutLargeIcon />
+        </IconButton>
+        <IconButton>
+           <ChatIcon />
+        </IconButton>
+        <IconButton>    
+           <MoreVertIcon />
+        </IconButton>
       </div>
      </div>
-     <div className="sidebar_search"></div>
+     <div className="sidebar_search">
+        <SearchOutlined />
+        <input placeholder="Search or start new chat" type="text" />
+     </div>
+
      <div className="sidebar_chats"></div>
     </div>
   );    
