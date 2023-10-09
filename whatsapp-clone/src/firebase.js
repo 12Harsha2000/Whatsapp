612bdad8a2801f296ff5cd40eb5274b06b6b5cb4
12Harsha2000 <harshamariajose.20@gmail.com>
2023-10-09 10:33:32 +0530

    Initiated

--- whatsapp-clone/src/firebase.js
+++ whatsapp-clone/src/firebase.js
@@ -1,21 +1,21 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDXVlRIhHEEaBhLJVeLhApJgxPIv00EWX0",
-    authDomain: "whatsapp-clone-2ac4e.firebaseapp.com",
-    projectId: "whatsapp-clone-2ac4e",
-    storageBucket: "whatsapp-clone-2ac4e.appspot.com",
-    messagingSenderId: "816726071264",
-    appId: "1:816726071264:web:efc8b1d190ff1ecd7b3d85",
-    measurementId: "G-PM6ZS9BYEX"
-  };
+  apiKey: "AIzaSyDXVlRIhHEEaBhLJVeLhApJgxPIv00EWX0",
+  authDomain: "whatsapp-clone-2ac4e.firebaseapp.com",
+  projectId: "whatsapp-clone-2ac4e",
+  storageBucket: "whatsapp-clone-2ac4e.appspot.com",
+  messagingSenderId: "816726071264",
+  appId: "1:816726071264:web:efc8b1d190ff1ecd7b3d85",
+  measurementId: "G-PM6ZS9BYEX"
+};
 
-  const firebaseapp = firebase.initializeApp(firebaseConfig);
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebaseApp.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  export { auth, provider };
-  export default db;
+export { auth, db, provider };
+export default firebaseApp;
