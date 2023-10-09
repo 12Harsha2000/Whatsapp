de788175c14b9c64db7904201dbde13f749699b9
12Harsha2000 <harshamariajose.20@gmail.com>
2023-10-09 12:23:41 +0530

    Initiated

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,27 +1,33 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from "./Chat";
 import{ BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return ( 
     <div className="App">
-     <div className="app_body">
-     <Router>
-      <Switch>
-      <Sidebar />
-
-
-        <Route path = "/rooms/:roomId">
-          <Chat />
-        </Route>
-         <Route path="/">
-          <Chat />
-         </Route>
-      </Switch>
-     </Router> 
-     </div>
+      {!user ? (
+        <h1>LOGIN</h1>
+      ) : (
+        <div className="app_body">
+        <Router>
+         <Switch>
+         <Sidebar />
+   
+   
+           <Route path = "/rooms/:roomId">
+             <Chat />
+           </Route>
+            <Route path="/">
+             <Chat />
+            </Route>
+         </Switch>
+        </Router> 
+        </div>
+      )}
     </div>
   );
 }
